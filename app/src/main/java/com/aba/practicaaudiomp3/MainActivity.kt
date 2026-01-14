cbbb05857257432a3dcc18f47287b62fb0b48709
ElGolondras <jfoncla2@institutodh.net>
2026-01-14 16:28:07 +0100

    Finish

---
+++ app/src/main/java/com/aba/practicaaudiomp3/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.aba.practicaaudiomp3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.aba.practicaaudiomp3.ui.theme.PracticaAudioMP3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            setContent {
+                MaterialTheme {
+                    AudioPracticeScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PracticaAudioMP3Theme {
+        Greeting("Android")
+    }
+}
